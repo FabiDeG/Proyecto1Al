9e581c053ca3b2e56625f3d819dd7fb4f8c84f37
Fabiola <105880646+FabiDeG@users.noreply.github.com>
2023-02-10 23:57:56 -0600

    Update FaC.lisp

--- FaC.lisp
+++ FaC.lisp
@@ -1,15 +1,12 @@
-(
-  defun FaCe()
-  (format t "~%Pasaremos farenheith a centigrados, dime los farenheith que seas convertir")
-  (read)
-)
+(defun FaCe ()
+  (format t "~%Pasaremos farenheith a centigrados, dime los farenheith que seas convertir: ")
+  (let ((fah (read)))
+    (values fah)))
 
-(
-  defun opera(fah)
-  (let (cen (* (- fah 32) (/ 9 5))
-    (format t "~%~s Fahrenheit son ~s grados Censius~%" fah (float cen))
+(defun opera (fah)
+  (let ((cen (* (- fah 32) (/ 9 5))))
+    (format t "%~s grados Fahrenheit son ~s grados Celsius%" fah (float cen))
     cen))
-)
 
-(let ((fah (FaCe))))
-(opera fah)
+(let ((fahr (multiple-value-bind (fahr) (FaCe))))
+  (opera fahr))
